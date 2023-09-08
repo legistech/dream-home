1b88c3f81d4f278f99c62896ab0f8c0a7d49009a
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-08 16:25:39 +0500

    🚀 Add home with bottom navigation bar containing main iui screens

--- lib/src/common/presentation/screens/home.dart
+++ lib/src/common/presentation/screens/home.dart
@@ -1,12 +1,36 @@
-import 'package:dream_home/src/features/authentication/application/blocs/login/login_bloc.dart';
+import 'package:dream_home/src/features/featured_properties/presentation/screens/fp.dart';
 import 'package:dream_home/src/theme/pellet.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iconly/iconly.dart';
 
 /// [HomeScreen] is the home page of the app.
-class HomeScreen extends StatelessWidget {
+/// It contains a [BottomNavigationBar] to navigate between the screens.
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+/// [screens] contains the list of screens to be displayed.
+/// [selectedIndex] is the index of the screen to be displayed.
+/// To add a new screen, add it to the [screens] list and add a new index to [selectedIndex].
+List<Widget> screens = [
+  const FeaturedPropertiesScreen(),
+  const Center(
+    child: Text('MapScreen'),
+  ),
+  const Center(
+    child: Text('InboxScreen'),
+  ),
+  const Center(
+    child: Text('ProfileScreen'),
+  ),
+];
+
+int selectedIndex = 0;
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,21 +38,62 @@ class HomeScreen extends StatelessWidget {
         gradient: Pellet.kBackgroundGradient,
       ),
       child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {
-                  // TODO: Remove this when proper logout is implemented
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: screens[selectedIndex],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Pellet.kPrimaryColor,
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 8,
+          child: SizedBox(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                navigationBarItem(IconlyLight.home, 0, "Home"),
+                navigationBarItem(Icons.map_outlined, 1, "Map"),
+                navigationBarItem(IconlyLight.chat, 2, "Inbox"),
+                navigationBarItem(IconlyLight.user, 3, "Profile"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-                  context.read<LoginBloc>().add(LogoutButtonPressed());
-                  Navigator.pushReplacementNamed(context, '/');
-                },
-                icon: const Icon(Icons.logout))
-          ],
-        ),
-        body: const Center(
-          child: Text('HomePage'),
-        ),
+  GestureDetector navigationBarItem(IconData icon, int index, String text) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        direction: Axis.vertical,
+        children: [
+          Icon(
+            icon,
+            color: selectedIndex != index
+                ? Pellet.kDark.withOpacity(0.5)
+                : Pellet.kDark,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              color: selectedIndex != index
+                  ? Pellet.kDark.withOpacity(0.5)
+                  : Pellet.kDark,
+            ),
+          )
+        ],
       ),
     );
   }
