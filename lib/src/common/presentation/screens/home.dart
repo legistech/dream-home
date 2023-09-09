0e81cd56ef621cc9fb40e9abb5f3b4e4641290bc
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-09 13:48:47 +0500

    ♻️ Fix presentation spelling

--- lib/src/common/presentation/screens/home.dart
+++ lib/src/common/presentation/screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:dream_home/src/constants/screen.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/screens/fp.dart';
-import 'package:dream_home/src/features/user_profile/presention/screens/profile.dart';
+import 'package:dream_home/src/features/user_profile/presentation/screens/profile.dart';
 import 'package:dream_home/src/theme/pellet.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
