87c46f044f3826ed470316fe4c86a0255de8bf05
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-10 20:10:59 +0500

    Add single property route

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -4,6 +4,7 @@ import 'package:dream_home/src/common/presentation/screens/splash.dart';
 import 'package:dream_home/src/common/presentation/screens/welcome.dart';
 import 'package:dream_home/src/features/authentication/presentation/screens/login.dart';
 import 'package:dream_home/src/features/authentication/presentation/screens/register.dart';
+import 'package:dream_home/src/features/featured_properties/presentation/screens/single_property.dart';
 import 'package:flutter/material.dart';
 
 /// [GenerateRoutes] class is used to generate routes for the app.
@@ -16,6 +17,20 @@ class GenerateRoutes {
       case '/welcome':
         return PageRouteBuilder(
           pageBuilder: (_, __, ___) => const WelcomeScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        );
+      case '/register':
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const RegisterScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        );
+      case '/login':
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => const LoginScreen(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             return FadeTransition(opacity: animation, child: child);
           },
@@ -33,20 +48,19 @@ class GenerateRoutes {
             },
           );
         }
-      case '/register':
-        return PageRouteBuilder(
-          pageBuilder: (_, __, ___) => const RegisterScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(opacity: animation, child: child);
-          },
-        );
-      case '/login':
-        return PageRouteBuilder(
-          pageBuilder: (_, __, ___) => const LoginScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(opacity: animation, child: child);
-          },
-        );
+      case '/single-property':
+        {
+          final args = settings.arguments as Map<String, dynamic>;
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) => SinglePropertyScreen(
+              property: args['property'],
+            ),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+          );
+        }
 
       default:
         return MaterialPageRoute(builder: (_) => const DreamHome());
