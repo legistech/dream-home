c527c9876d2afed9dd2101acae4b000462a9b79d
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-10 20:44:13 +0500

    Add property model

--- lib/src/features/featured_properties/domain/models/property.dart
+++ lib/src/features/featured_properties/domain/models/property.dart
@@ -1,3 +1,4 @@
+import 'package:dream_home/src/common/domain/models/user.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:pocketbase/pocketbase.dart';
 
@@ -6,88 +7,91 @@ part 'property.g.dart';
 @JsonSerializable()
 class Property {
   @JsonKey(name: "address")
-  final String address;
+  final String? address;
   @JsonKey(name: "amenities")
-  final List<String> amenities;
+  final List<String>? amenities;
   @JsonKey(name: "balcony")
-  final String balcony;
+  final String? balcony;
   @JsonKey(name: "bathRoom")
-  final String bathRoom;
+  final String? bathRoom;
   @JsonKey(name: "bedRoom")
-  final String bedRoom;
+  final String? bedRoom;
   @JsonKey(name: "collectionId")
-  final String collectionId;
+  final String? collectionId;
   @JsonKey(name: "collectionName")
-  final String collectionName;
+  final String? collectionName;
   @JsonKey(name: "contactNumber")
-  final int contactNumber;
+  final int? contactNumber;
   @JsonKey(name: "created")
-  final DateTime created;
+  final DateTime? created;
   @JsonKey(name: "createdBy")
-  final String createdBy;
+  final String? createdBy;
+  @JsonKey(name: "expand")
+  final Expand? expand;
   @JsonKey(name: "floorNum")
-  final String floorNum;
+  final String? floorNum;
   @JsonKey(name: "furnitureStatus")
-  final String furnitureStatus;
+  final String? furnitureStatus;
   @JsonKey(name: "id")
-  final String id;
+  final String? id;
   @JsonKey(name: "images")
-  final List<String> images;
+  final List<String>? images;
   @JsonKey(name: "isCoveredParking")
-  final bool isCoveredParking;
+  final bool? isCoveredParking;
   @JsonKey(name: "isPetFriendly")
-  final bool isPetFriendly;
+  final bool? isPetFriendly;
   @JsonKey(name: "isResidential")
-  final bool isResidential;
+  final bool? isResidential;
   @JsonKey(name: "isSelling")
-  final bool isSelling;
+  final bool? isSelling;
   @JsonKey(name: "isUnderConstruction")
-  final bool isUnderConstruction;
+  final bool? isUnderConstruction;
   @JsonKey(name: "propertyCost")
-  final int propertyCost;
+  final int? propertyCost;
   @JsonKey(name: "propertyDetail")
-  final String propertyDetail;
+  final String? propertyDetail;
   @JsonKey(name: "propertyName")
-  final String propertyName;
+  final String? propertyName;
   @JsonKey(name: "propertyNum")
-  final String propertyNum;
+  final String? propertyNum;
   @JsonKey(name: "propertySize")
-  final int propertySize;
+  final int? propertySize;
   @JsonKey(name: "propertyType")
-  final String propertyType;
+  final String? propertyType;
   @JsonKey(name: "updated")
-  final DateTime updated;
+  final DateTime? updated;
   @JsonKey(name: "userStatus")
-  final String userStatus;
+  final String? userStatus;
 
   Property({
-    required this.address,
-    required this.amenities,
-    required this.balcony,
-    required this.bathRoom,
-    required this.bedRoom,
-    required this.collectionId,
-    required this.collectionName,
-    required this.contactNumber,
-    required this.created,
-    required this.createdBy,
-    required this.floorNum,
-    required this.furnitureStatus,
-    required this.id,
-    required this.images,
-    required this.isCoveredParking,
-    required this.isPetFriendly,
-    required this.isResidential,
-    required this.isSelling,
-    required this.isUnderConstruction,
-    required this.propertyCost,
-    required this.propertyDetail,
-    required this.propertyName,
-    required this.propertyNum,
-    required this.propertySize,
-    required this.propertyType,
-    required this.updated,
-    required this.userStatus,
+    this.address,
+    this.amenities,
+    this.balcony,
+    this.bathRoom,
+    this.bedRoom,
+    this.collectionId,
+    this.collectionName,
+    this.contactNumber,
+    this.created,
+    this.createdBy,
+    this.expand,
+    this.floorNum,
+    this.furnitureStatus,
+    this.id,
+    this.images,
+    this.isCoveredParking,
+    this.isPetFriendly,
+    this.isResidential,
+    this.isSelling,
+    this.isUnderConstruction,
+    this.propertyCost,
+    this.propertyDetail,
+    this.propertyName,
+    this.propertyNum,
+    this.propertySize,
+    this.propertyType,
+    this.updated,
+    this.userStatus,
   });
 
   Property copyWith({
@@ -101,6 +105,7 @@ class Property {
     int? contactNumber,
     DateTime? created,
     String? createdBy,
+    Expand? expand,
     String? floorNum,
     String? furnitureStatus,
     String? id,
@@ -130,6 +135,7 @@ class Property {
         contactNumber: contactNumber ?? this.contactNumber,
         created: created ?? this.created,
         createdBy: createdBy ?? this.createdBy,
+        expand: expand ?? this.expand,
         floorNum: floorNum ?? this.floorNum,
         furnitureStatus: furnitureStatus ?? this.furnitureStatus,
         id: id ?? this.id,
@@ -156,4 +162,28 @@ class Property {
       _$PropertyFromJson(json);
 
   Map<String, dynamic> toJson() => _$PropertyToJson(this);
+}
+
+@JsonSerializable()
+class Expand {
+  @JsonKey(name: "createdBy")
+  final User? createdBy;
+
+  Expand({
+    this.createdBy,
+  });
+
+  Expand copyWith({
+    User? createdBy,
+  }) =>
+      Expand(
+        createdBy: createdBy ?? this.createdBy,
+      );
+
+  factory Expand.fromRecord(RecordModel record) =>
+      Expand.fromJson(record.toJson());
+
+  factory Expand.fromJson(Map<String, dynamic> json) => _$ExpandFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ExpandToJson(this);
 }
--- lib/src/features/featured_properties/domain/models/property.g.dart
+++ lib/src/features/featured_properties/domain/models/property.g.dart
@@ -7,35 +7,43 @@ part of 'property.dart';
 // **************************************************************************
 
 Property _$PropertyFromJson(Map<String, dynamic> json) => Property(
-      address: json['address'] as String,
-      amenities:
-          (json['amenities'] as List<dynamic>).map((e) => e as String).toList(),
-      balcony: json['balcony'] as String,
-      bathRoom: json['bathRoom'] as String,
-      bedRoom: json['bedRoom'] as String,
-      collectionId: json['collectionId'] as String,
-      collectionName: json['collectionName'] as String,
-      contactNumber: json['contactNumber'] as int,
-      created: DateTime.parse(json['created'] as String),
-      createdBy: json['createdBy'] as String,
-      floorNum: json['floorNum'] as String,
-      furnitureStatus: json['furnitureStatus'] as String,
-      id: json['id'] as String,
+      address: json['address'] as String?,
+      amenities: (json['amenities'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      balcony: json['balcony'] as String?,
+      bathRoom: json['bathRoom'] as String?,
+      bedRoom: json['bedRoom'] as String?,
+      collectionId: json['collectionId'] as String?,
+      collectionName: json['collectionName'] as String?,
+      contactNumber: json['contactNumber'] as int?,
+      created: json['created'] == null
+          ? null
+          : DateTime.parse(json['created'] as String),
+      createdBy: json['createdBy'] as String?,
+      expand: json['expand'] == null
+          ? null
+          : Expand.fromJson(json['expand'] as Map<String, dynamic>),
+      floorNum: json['floorNum'] as String?,
+      furnitureStatus: json['furnitureStatus'] as String?,
+      id: json['id'] as String?,
       images:
-          (json['images'] as List<dynamic>).map((e) => e as String).toList(),
-      isCoveredParking: json['isCoveredParking'] as bool,
-      isPetFriendly: json['isPetFriendly'] as bool,
-      isResidential: json['isResidential'] as bool,
-      isSelling: json['isSelling'] as bool,
-      isUnderConstruction: json['isUnderConstruction'] as bool,
-      propertyCost: json['propertyCost'] as int,
-      propertyDetail: json['propertyDetail'] as String,
-      propertyName: json['propertyName'] as String,
-      propertyNum: json['propertyNum'] as String,
-      propertySize: json['propertySize'] as int,
-      propertyType: json['propertyType'] as String,
-      updated: DateTime.parse(json['updated'] as String),
-      userStatus: json['userStatus'] as String,
+          (json['images'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      isCoveredParking: json['isCoveredParking'] as bool?,
+      isPetFriendly: json['isPetFriendly'] as bool?,
+      isResidential: json['isResidential'] as bool?,
+      isSelling: json['isSelling'] as bool?,
+      isUnderConstruction: json['isUnderConstruction'] as bool?,
+      propertyCost: json['propertyCost'] as int?,
+      propertyDetail: json['propertyDetail'] as String?,
+      propertyName: json['propertyName'] as String?,
+      propertyNum: json['propertyNum'] as String?,
+      propertySize: json['propertySize'] as int?,
+      propertyType: json['propertyType'] as String?,
+      updated: json['updated'] == null
+          ? null
+          : DateTime.parse(json['updated'] as String),
+      userStatus: json['userStatus'] as String?,
     );
 
 Map<String, dynamic> _$PropertyToJson(Property instance) => <String, dynamic>{
@@ -47,8 +55,9 @@ Map<String, dynamic> _$PropertyToJson(Property instance) => <String, dynamic>{
       'collectionId': instance.collectionId,
       'collectionName': instance.collectionName,
       'contactNumber': instance.contactNumber,
-      'created': instance.created.toIso8601String(),
+      'created': instance.created?.toIso8601String(),
       'createdBy': instance.createdBy,
+      'expand': instance.expand,
       'floorNum': instance.floorNum,
       'furnitureStatus': instance.furnitureStatus,
       'id': instance.id,
@@ -64,6 +73,16 @@ Map<String, dynamic> _$PropertyToJson(Property instance) => <String, dynamic>{
       'propertyNum': instance.propertyNum,
       'propertySize': instance.propertySize,
       'propertyType': instance.propertyType,
-      'updated': instance.updated.toIso8601String(),
+      'updated': instance.updated?.toIso8601String(),
       'userStatus': instance.userStatus,
+    };
+
+Expand _$ExpandFromJson(Map<String, dynamic> json) => Expand(
+      createdBy: json['createdBy'] == null
+          ? null
+          : User.fromJson(json['createdBy'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ExpandToJson(Expand instance) => <String, dynamic>{
+      'createdBy': instance.createdBy,
     };
