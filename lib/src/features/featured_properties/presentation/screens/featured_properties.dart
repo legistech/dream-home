b66d0ebf04809d72bdfdc0ea1544f3c64b365a0e
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-10 23:37:53 +0500

    Add search controller

--- lib/src/features/featured_properties/presentation/screens/featured_properties.dart
+++ lib/src/features/featured_properties/presentation/screens/featured_properties.dart
@@ -1,6 +1,7 @@
 import 'package:dream_home/src/constants/screen.dart';
 import 'package:dream_home/src/features/featured_properties/domain/models/property.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/views/app_bar.dart';
+import 'package:dream_home/src/features/featured_properties/presentation/views/filters_bottom_sheet.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/views/property_list.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/widgets/common_choice_chip.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/widgets/featured_image.dart';
@@ -9,10 +10,32 @@ import 'package:dream_home/src/theme/pellet.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 
-class FeaturedPropertiesScreen extends StatelessWidget {
+class FeaturedPropertiesScreen extends StatefulWidget {
   const FeaturedPropertiesScreen({super.key, required this.properties});
   final List<Property> properties;
+
+  @override
+  State<FeaturedPropertiesScreen> createState() =>
+      _FeaturedPropertiesScreenState();
+}
+
+class _FeaturedPropertiesScreenState extends State<FeaturedPropertiesScreen> {
   final String baseUrl = 'https://dream-home.pockethost.io/api/files/';
+
+  late final TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = ScreenSize.height(context);
@@ -24,35 +47,9 @@ class FeaturedPropertiesScreen extends StatelessWidget {
         SizedBox(height: height * 1),
         Row(
           children: [
-            Expanded(
-              child: SizedBox(
-                height: 50,
-                child: TextField(
-                  keyboardType: TextInputType.text,
-                  onTapOutside: (event) =>
-                      FocusManager.instance.primaryFocus?.unfocus(),
-                  decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                      hintText: 'Search for anything',
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide.none,
-                      ),
-                      prefixIcon: Icon(
-                        IconlyLight.search,
-                        color: Pellet.kDark,
-                      ),
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          IconlyLight.filter,
-                          color: Pellet.kDark,
-                        ),
-                      )),
-                ),
-              ),
+            CommonFeaturesSearchField(
+              showFilters: true,
+              searchController: _searchController,
             ),
           ],
         ),
@@ -81,7 +78,7 @@ class FeaturedPropertiesScreen extends StatelessWidget {
         ),
         SizedBox(height: height * 2),
         FeaturedImage(
-          '$baseUrl/${properties.first.collectionId}/${properties.first.id}/${properties.first.images!.first}',
+          '$baseUrl/${widget.properties.first.collectionId}/${widget.properties.first.id}/${widget.properties.first.images!.first}',
           borderColor: Pellet.kPrimaryColor,
         ),
         SizedBox(height: height * 2),
@@ -93,14 +90,89 @@ class FeaturedPropertiesScreen extends StatelessWidget {
               context,
               '/view-all',
               arguments: {
-                'properties': properties,
+                'properties': widget.properties,
               },
             );
           },
         ),
         SizedBox(height: height * 1),
-        PropertyListView(properties: properties, baseUrl: baseUrl),
+        PropertyListView(properties: widget.properties, baseUrl: baseUrl),
       ],
+    );
+  }
+}
+
+class CommonFeaturesSearchField extends StatelessWidget {
+  const CommonFeaturesSearchField({
+    super.key,
+    this.showFilters = false,
+    required TextEditingController searchController,
+    this.borderColor = Colors.transparent,
+  }) : _searchController = searchController;
+
+  final TextEditingController _searchController;
+  final bool showFilters;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: SizedBox(
+        height: 50,
+        child: TextField(
+          controller: _searchController,
+          keyboardType: TextInputType.text,
+          onTapOutside: (event) =>
+              FocusManager.instance.primaryFocus?.unfocus(),
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            hintText: 'Search for anything',
+            filled: true,
+            fillColor: Colors.white,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(
+                color: borderColor,
+                width: 2.0,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(
+                color: borderColor,
+                width: 2.0,
+              ),
+            ),
+            prefixIcon: Icon(
+              IconlyLight.search,
+              color: Pellet.kDark,
+            ),
+            suffixIcon: showFilters
+                ? IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => const FiltersBottomSheet(),
+                        isScrollControlled: true,
+                        showDragHandle: true,
+                        backgroundColor: Pellet.kWhite,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            topRight: Radius.circular(16),
+                          ),
+                        ),
+                      );
+                    },
+                    icon: Icon(
+                      IconlyLight.filter,
+                      color: Pellet.kDark,
+                    ),
+                  )
+                : null,
+          ),
+        ),
+      ),
     );
   }
 }
