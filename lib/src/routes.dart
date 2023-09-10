bbffc7ea5b12981848f08764b5e092753f1acfe7
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-10 20:11:44 +0500

    Add view all properties route

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -5,6 +5,7 @@ import 'package:dream_home/src/common/presentation/screens/welcome.dart';
 import 'package:dream_home/src/features/authentication/presentation/screens/login.dart';
 import 'package:dream_home/src/features/authentication/presentation/screens/register.dart';
 import 'package:dream_home/src/features/featured_properties/presentation/screens/single_property.dart';
+import 'package:dream_home/src/features/featured_properties/presentation/screens/view_all.dart';
 import 'package:flutter/material.dart';
 
 /// [GenerateRoutes] class is used to generate routes for the app.
@@ -61,7 +62,19 @@ class GenerateRoutes {
             },
           );
         }
-
+      case '/view-all':
+        {
+          final args = settings.arguments as Map<String, dynamic>;
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) => ViewAllPropertiesScreen(
+              properties: args['properties'],
+            ),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+          );
+        }
       default:
         return MaterialPageRoute(builder: (_) => const DreamHome());
     }
