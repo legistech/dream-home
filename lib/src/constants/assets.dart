a33f826e2349c0f3c5996cb914484b304978c6b7
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-11 13:08:17 +0500

    Add random featured images

--- lib/src/constants/assets.dart
+++ lib/src/constants/assets.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 /// [Asset] is used to store and retrieve all
 /// the assets strings used in the app.
 /// use [instance] getter to get the instance of [Asset]
@@ -16,16 +14,6 @@ class Asset {
   static String get calculatorIcon => 'assets/icons/calculator.png';
   static String get increaseIcon => 'assets/icons/increase.png';
   static String get decreaseIcon => 'assets/icons/decrease.png';
-  static String get featuredImage {
-    final images = [
-      'https://www.aurecongroup.com/-/media/images/aurecon/content/insights/botf_featured-insights_buildings-of-the-future/building-of-the-future-desktop-v2.jpg?as=0&w=2048',
-      'https://www.shapesbyhydro.com/globalassets/shapes/expert-thoughts/materials-for-supertall-buildings01.jpg'
-          'https://architizer-prod.imgix.net/media/1392288811491Executive_Arch_Interlace_Photo_1.jpg'
-          'https://images.timesproperty.com/blog/2347/unnamed___2022_06_14T183130168.jpg'
-    ];
-
-    int randomIndex = Random().nextInt(images.length);
-
-    return images[randomIndex];
-  }
+  static String get featuredImage =>
+      'https://source.unsplash.com/480x480/?property';
 }
