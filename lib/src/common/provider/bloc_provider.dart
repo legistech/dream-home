bc684b4c12142a4acbe66d8b0416ee5e54c05199
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-11 11:41:39 +0500

    Add featured properties bloc #9

--- lib/src/common/provider/bloc_provider.dart
+++ lib/src/common/provider/bloc_provider.dart
@@ -1,6 +1,7 @@
 import 'package:dream_home/src/features/authentication/application/blocs/forgot_pwd/forgot_pwd_bloc.dart';
 import 'package:dream_home/src/features/authentication/application/blocs/login/login_bloc.dart';
 import 'package:dream_home/src/features/authentication/application/blocs/register/register_bloc.dart';
+import 'package:dream_home/src/features/featured_properties/application/blocs/bloc/featured_properties_bloc.dart';
 import 'package:dream_home/src/features/user_profile/application/blocs/profile_bloc/profile_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,5 +11,7 @@ class Providers {
         BlocProvider<RegisterBloc>(create: (context) => RegisterBloc()),
         BlocProvider<ForgotPwdBloc>(create: (context) => ForgotPwdBloc()),
         BlocProvider<ProfileBloc>(create: (context) => ProfileBloc()),
+        BlocProvider<FeaturedPropertiesBloc>(
+            create: (context) => FeaturedPropertiesBloc()),
       ];
 }
