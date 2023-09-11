1f3cb6aed0bb6bec099e1be04ae5f4354158f5ba
Haxeeb71 <135656763+Haxeeb71@users.noreply.github.com>
2023-09-11 15:47:58 +0500

    Change createproperty widget name

--- lib/src/features/create_properties/presentation/screens/create_properties.dart
+++ lib/src/features/create_properties/presentation/screens/create_properties.dart
@@ -1,8 +1,8 @@
 import 'package:dream_home/src/theme/pellet.dart';
 import 'package:flutter/material.dart';
 
-class CreateProperty extends StatelessWidget {
-  const CreateProperty({super.key});
+class CreatePropertyScreen extends StatelessWidget {
+  const CreatePropertyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,14 @@ class CreateProperty extends StatelessWidget {
             ),
           ],
         ),
-        body: Column(),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('step 1 of 1'),
+            )
+          ],
+        ),
         // bottomNavigationBar: BottomAppBar(
         //   child: CommonElevatedButton(text: 'Next'),
         // ),
