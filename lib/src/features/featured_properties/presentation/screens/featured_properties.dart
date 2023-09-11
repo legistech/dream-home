29dc95ec19cea8edc2c1cc136cd5271b38aff467
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-11 22:43:20 +0500

    Move featured properties to column from list view

--- lib/src/features/featured_properties/presentation/screens/featured_properties.dart
+++ lib/src/features/featured_properties/presentation/screens/featured_properties.dart
@@ -1,4 +1,5 @@
 import 'package:chip_list/chip_list.dart';
+import 'package:dream_home/src/features/featured_properties/presentation/widgets/property_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconly/iconly.dart';
@@ -9,7 +10,6 @@ import '../../../../theme/pellet.dart';
 import '../../application/blocs/bloc/featured_properties_bloc.dart';
 import '../../domain/models/property.dart';
 import '../views/app_bar.dart';
-import '../views/property_list.dart';
 import '../widgets/featured_image.dart';
 import '../widgets/featured_info.dart';
 import '../widgets/search_properties_field.dart';
@@ -52,74 +52,88 @@ class _FeaturedPropertiesScreenState extends State<FeaturedPropertiesScreen> {
           filteredProperties = state.properties;
         }
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: height * 2),
-          const AppBarView(),
-          SizedBox(height: height * 1),
-          CommonFeaturesSearchField(
-            showFilters: true,
-            searchController: _searchController,
-          ),
-          SizedBox(height: height * 1),
-          ChipList(
-            listOfChipNames: filterChip,
-            listOfChipIndicesCurrentlySeclected: selectedFilter,
-            activeBgColorList: [Pellet.kPrimaryColor],
-            inactiveTextColorList: [Pellet.kDark],
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-            extraOnToggle: (index) {
-              // Using setState as it is required by the library to rebuild the ui
-              setState(() {
-                context.read<FeaturedPropertiesBloc>().add(
-                      FeaturedFilterChipSelected(
-                        properties: widget.properties,
-                        index: index,
-                      ),
-                    );
-              });
-            },
-          ),
-          SizedBox(height: height * 2),
-          if (filteredProperties.isNotEmpty) ...[
-            FeaturedImage(
-              Asset.featuredImage,
-              borderColor: Pellet.kPrimaryColor,
-            ),
-            SizedBox(height: height * 2),
-            FeaturedInfo(
-              leading: 'Featured Properties',
-              trailing: 'View All',
-              onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  '/view-all',
-                  arguments: {
-                    'properties': widget.properties,
-                  },
-                );
-              },
-            ),
-            SizedBox(height: height * 1),
-            PropertyListView(properties: filteredProperties, baseUrl: baseUrl),
-          ] else ...[
-            const Spacer(),
-            Icon(IconlyLight.filter, size: 50, color: Pellet.kDark),
-            const SizedBox(height: 16),
-            const Text(
-              'No properties found',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const AppBarView(),
+              SizedBox(height: height * 1),
+              CommonFeaturesSearchField(
+                showFilters: true,
+                searchController: _searchController,
               ),
-            ),
-            const Spacer(),
-          ],
-        ],
+              SizedBox(height: height * 1),
+              ChipList(
+                listOfChipNames: filterChip,
+                listOfChipIndicesCurrentlySeclected: selectedFilter,
+                activeBgColorList: [Pellet.kPrimaryColor],
+                inactiveTextColorList: [Pellet.kDark],
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                extraOnToggle: (index) {
+                  // Using setState as it is required by the library to rebuild the ui
+                  setState(() {
+                    context.read<FeaturedPropertiesBloc>().add(
+                          FeaturedFilterChipSelected(
+                            properties: widget.properties,
+                            index: index,
+                          ),
+                        );
+                  });
+                },
+              ),
+              SizedBox(height: height * 2),
+              if (filteredProperties.isNotEmpty) ...[
+                FeaturedImage(
+                  Asset.featuredImage,
+                  borderColor: Pellet.kPrimaryColor,
+                ),
+                SizedBox(height: height * 2),
+                FeaturedInfo(
+                  leading: 'Featured Properties',
+                  trailing: 'View All',
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/view-all',
+                      arguments: {
+                        'properties': widget.properties,
+                      },
+                    );
+                  },
+                ),
+                SizedBox(height: height * 1),
+                // PropertyListView(properties: filteredProperties, baseUrl: baseUrl),
+                ...filteredProperties.map((property) {
+                  return Column(
+                    children: [
+                      PropertyCard(
+                        property: property,
+                        baseUrl: baseUrl,
+                      ),
+                      SizedBox(height: height * 1),
+                    ],
+                  );
+                }).toList(),
+              ] else ...[
+                const Spacer(),
+                Icon(IconlyLight.filter, size: 50, color: Pellet.kDark),
+                const SizedBox(height: 16),
+                const Text(
+                  'No properties found',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ],
+          ),
+        ),
       ),
     );
   }
