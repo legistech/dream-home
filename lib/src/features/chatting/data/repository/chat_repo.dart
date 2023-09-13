5674fb080ce3ab2435bde687d6a777e53c7a5ab2
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-09-13 22:05:56 +0500

    feat: Add load chats, load messages, create chat methods

--- lib/src/features/chatting/data/repository/chat_repo.dart
+++ lib/src/features/chatting/data/repository/chat_repo.dart
@@ -27,6 +27,7 @@ class ChatRepository {
     }
   }
 
+  /// [loadChats] returns a list of chats matching the current user
   static Future<ResultList<RecordModel>> loadChats() async {
     try {
       final pb = await PocketBaseInstance.instance;
@@ -42,6 +43,22 @@ class ChatRepository {
     }
   }
 
+  /// [loadMessages] returns a list of messages matching the chat with [chatId]
+  static Future<ResultList<RecordModel>> loadMessages(String chatId) async {
+    try {
+      final pb = await PocketBaseInstance.instance;
+      final record = await pb.collection('messages').getList(
+        headers: {'token': 'dev'},
+        filter: 'chat = "$chatId"',
+      );
+      return record;
+    } on ClientException {
+      rethrow;
+    }
+  }
+
+  /// [createChat] creates a new chat between the current user and the user with [userId]
+  /// if chat between these users already exists, it returns the id of the existing chat
   static Future<String> createChat(String userId) async {
     try {
       final pb = await PocketBaseInstance.instance;
