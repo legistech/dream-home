eee584942b0bd66ecf9cd3670767f106529ba158
Dervaishe <dervaishabbas@gmail.com>
2023-09-16 16:12:15 +0500

    Add padding

--- lib/src/features/featured_properties/presentation/screens/featured_properties.dart
+++ lib/src/features/featured_properties/presentation/screens/featured_properties.dart
@@ -45,93 +45,95 @@ class _FeaturedPropertiesScreenState extends State<FeaturedPropertiesScreen> {
   @override
   Widget build(BuildContext context) {
     final height = ScreenSize.height(context);
-
-    return BlocListener<FeaturedPropertiesBloc, FeaturedPropertiesState>(
-      listener: (context, state) {
-        if (state is FeaturedFilteredProperties) {
-          filteredProperties = state.properties;
-        }
-      },
-      child: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const AppBarView(),
-              SizedBox(height: height * 1),
-              CommonFeaturesSearchField(
-                showFilters: true,
-                searchController: _searchController,
-                hintText: 'Search for anything',
-              ),
-              SizedBox(height: height * 1),
-              ChipList(
-                listOfChipNames: filterChip,
-                listOfChipIndicesCurrentlySeclected: selectedFilter,
-                activeBgColorList: [Pellet.kPrimaryColor],
-                inactiveTextColorList: [Pellet.kDark],
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-                extraOnToggle: (index) {
-                  // Using setState as it is required by the library to rebuild the ui
-                  setState(() {
-                    context.read<FeaturedPropertiesBloc>().add(
-                          FeaturedFilterChipSelected(
-                            properties: widget.properties,
-                            index: index,
-                          ),
-                        );
-                  });
-                },
-              ),
-              SizedBox(height: height * 2),
-              if (filteredProperties.isNotEmpty) ...[
-                FeaturedImage(
-                  Asset.featuredImage,
-                  borderColor: Pellet.kPrimaryColor,
-                ),
-                SizedBox(height: height * 2),
-                FeaturedInfo(
-                  leading: 'Featured Properties',
-                  trailing: 'View All',
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/view-all',
-                      arguments: {
-                        'properties': widget.properties,
-                      },
-                    );
-                  },
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: BlocListener<FeaturedPropertiesBloc, FeaturedPropertiesState>(
+        listener: (context, state) {
+          if (state is FeaturedFilteredProperties) {
+            filteredProperties = state.properties;
+          }
+        },
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const AppBarView(),
+                SizedBox(height: height * 1),
+                CommonFeaturesSearchField(
+                  showFilters: true,
+                  searchController: _searchController,
+                  hintText: 'Search for anything',
                 ),
                 SizedBox(height: height * 1),
-                // PropertyListView(properties: filteredProperties, baseUrl: baseUrl),
-                ...filteredProperties.map((property) {
-                  return Column(
-                    children: [
-                      PropertyCard(
-                        property: property,
-                        baseUrl: baseUrl,
-                      ),
-                      SizedBox(height: height * 1),
-                    ],
-                  );
-                }).toList(),
-                SizedBox(height: height * 2),
-              ] else ...[
-                Icon(IconlyLight.filter, size: 50, color: Pellet.kDark),
-                const SizedBox(height: 16),
-                const Text(
-                  'Nothing to see here',
-                  style: TextStyle(
+                ChipList(
+                  listOfChipNames: filterChip,
+                  listOfChipIndicesCurrentlySeclected: selectedFilter,
+                  activeBgColorList: [Pellet.kPrimaryColor],
+                  inactiveTextColorList: [Pellet.kDark],
+                  style: const TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
+                  extraOnToggle: (index) {
+                    // Using setState as it is required by the library to rebuild the ui
+                    setState(() {
+                      context.read<FeaturedPropertiesBloc>().add(
+                            FeaturedFilterChipSelected(
+                              properties: widget.properties,
+                              index: index,
+                            ),
+                          );
+                    });
+                  },
                 ),
+                SizedBox(height: height * 2),
+                if (filteredProperties.isNotEmpty) ...[
+                  FeaturedImage(
+                    Asset.featuredImage,
+                    borderColor: Pellet.kPrimaryColor,
+                  ),
+                  SizedBox(height: height * 2),
+                  FeaturedInfo(
+                    leading: 'Featured Properties',
+                    trailing: 'View All',
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/view-all',
+                        arguments: {
+                          'properties': widget.properties,
+                        },
+                      );
+                    },
+                  ),
+                  SizedBox(height: height * 1),
+                  // PropertyListView(properties: filteredProperties, baseUrl: baseUrl),
+                  ...filteredProperties.map((property) {
+                    return Column(
+                      children: [
+                        PropertyCard(
+                          property: property,
+                          baseUrl: baseUrl,
+                        ),
+                        SizedBox(height: height * 1),
+                      ],
+                    );
+                  }).toList(),
+                  SizedBox(height: height * 2),
+                ] else ...[
+                  Icon(IconlyLight.filter, size: 50, color: Pellet.kDark),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Nothing to see here',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
